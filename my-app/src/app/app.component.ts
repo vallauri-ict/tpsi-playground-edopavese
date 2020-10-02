2c284d3573cf7c12dece8bdc85deb397f172e6c0
edopavese <71269379+edopavese@users.noreply.github.com>
2020-10-02 13:00:16 +0200

    Funzione di nomi casuali aggiunta

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  names = ["Edoardo", "Camilla", "Roberta", "Filippo", "Maria", "Jennifer", "Chica Mala", "Sandra Bullock"];
+  myName = "Edo";
+
+  changeName(){
+    this.myName = this.names[Math.floor(Math.random()*8)];
+  }
 }
